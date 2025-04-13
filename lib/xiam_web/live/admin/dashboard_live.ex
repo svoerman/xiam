9c339c95f58a06349bdc28beef5a5229bff7f9f0
Sander Voerman <sander.voerman@kabisa.nl>
2025-04-13 13:20:34 +0200

    Update the admin page

--- lib/xiam_web/live/admin/dashboard_live.ex
+++ lib/xiam_web/live/admin/dashboard_live.ex
@@ -29,7 +29,6 @@ defmodule XIAMWeb.Admin.DashboardLive do
           show_back_link={false}
         />
       </div>
-      <.page_title page_title={@page_title} />
 
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <!-- Users Management Card -->
